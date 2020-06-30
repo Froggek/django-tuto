32a4f5278aadd561ec26d5d46a329ef878813635
froggek <froggek@gmail.com>
2020-06-30 19:27:01 +0200

    End of tuto/Part 1

--- polls/urls.py
+++ polls/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # route: URL pattern; first match is picked
+    # view: when a pattern is matched, a VIEW function is called
+    # w/ an HttpRequest obj as argument [+ captured values captures from the route, as KW arguments]
+    # [kwargs]: Arbitrary KW args passed to the view
+    # [name]: Unambiguous name for the URL, can be used in templates e.g.
     path('', views.index, name='index')
 ]
