e8ebad3d3d735ca97a6d59316f368b10c197d675
froggek <froggek@gmail.com>
2020-07-03 15:04:37 +0200

    Playing w/ the objects

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 
 class Question(models.Model):
@@ -7,9 +10,20 @@ class Question(models.Model):
     # Human-readable name
     pub_date = models.DateTimeField('date published')
 
+    # __str__ isn important for:
+    # the interactive prompt
+    # the admin part
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.choice_text
